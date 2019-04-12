e1c7dd2ce9a9f0d66aaa93865f55fc134250ea8d
José Manuel Agúndez García <jmagundezg@gmail.com>
2019-04-12 02:42:22 +0200

    Version 0.1

--- json_to_server.py
+++ json_to_server.py
@@ -12,6 +12,7 @@ db = {}
     POST: 
     DELETE: Erase a main json key
 """
+
 class MainRest(Resource):
     def get(self):
         """ 
@@ -35,49 +36,156 @@ class MainRest(Resource):
             print("[INFO] "+name+" table doesn't exists")
             return "[INFO] "+name+" table doesn't exists", 404
     
+    
     def post(self):
         """
             Input a table into the DB.
             If you use an already created table you will erase it
-            Example: http httpbin.org/post user:='{"name": "john", "age": 10 }
+            Example: http http://localhost:5000 user:='{"name": "john", "age": 10 }
         """
 
         json_data = request.get_json(force=True)
         if json_data is not None:
             key = list(json_data.keys())[0]
             db[key] = json_data[key]
-            return 201
+            return "[INFO] CREATED", 201
         else:
-            return 500
+            return "[ERROR] INTERNAL SERVER ERROR", 500
+
 
 class TableRest(Resource):
+    """
+        All the API refered to the tables.
+        URL: http://localhost:5000/<table_name>
+
+    """
+
     def get(self, key):
+        """ 
+        Returns the table named key
+        """
         if key in db.keys():
             return db[key], 200
         else:
-            return "[INFO] "+key+" table doesn't exists"
+            return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 404
 
     def post(self, key):
-        pass
+        """
+            Post a tuple into the $key table.
+            If you use an already created tuple you can't post it
+            Example: http http://localhost:5000/users :='{"name": "john", "age": 10 }
+        """
+        if key not in db.keys():
+            return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 404
+        json_data = request.get_json(force=True)
+        if json_data is not None:
+            ik = list(json_data.keys())[0]
+            data = json_data[ik]
+            if type(data) == dict:
+                if len(db[key]) == 0: 
+                    db[key].append(data)
+                    return "[INFO] ACCEPTED"201
+                elif data.keys() == db[key][0].keys():
+                    if len(list(filter(lambda x: x["id"] == data["id"], db[key]))) == 0:
+                        db[key].append(data)
+                else:
+                    return "[ERROR] WRONG COLUMNS FOR THAT TABLE", 403 
+            else:
+                return "[ERROR] INTERNAL SERVER ERROR", 500
+        return "[ERROR] INTERNAL SERVER ERROR", 500
+
+
 
     def delete(self, key):
+        """
+            Deletes a table if it exists
+        """
         print("[INFO] Trying to delete "+key+" table")
         if key in db.keys():
             print("[INFO] deleted "+key+" table")
             del db[key]
             return "[INFO] deleted "+key+" table", 200
-            
         else:
             print("[INFO] "+key+" table doesn't exists")
-            return "[INFO] "+ key +" table doesn't exists", 404
+            return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 404
 
 
 class TupleRest(Resource):
+    """
+        All the API refered to tuples
+        URL: http://localhost:5000/<table>/<id>
+    """
+
     def get(self, table, key):
+        """
+            Get the tuple with id=$key from the table=$table 
+        """
         if table in db.keys():
             id = 'id'
             tuples = list(filter(lambda x: (x[id] == key), db[table]))
-            return tuples, 200
+            return tuples[0], 200
+
+    def post(self, table, key):
+        """
+            Post a tuple into the $key table.
+            If you use an already created tuple you can't post it
+            The key variable is useless ¯\_(ツ)_/¯ 
+            Example: http http://localhost:5000/users/0 :='{"name": "john", "age": 10 }
+        """
+        if table not in db.keys():
+           return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 404
+
+        json_data = request.get_json(force=True)
+        if json_data is not None:
+            ik = list(json_data.keys())[0]
+            data = json_data[ik]
+            if type(data) == dict:
+                if len(db[key]) == 0: 
+                    db[key].append(data)
+                    return 201
+                elif data.keys() == db[key][0].keys():
+                    if len(list(filter(lambda x: x["id"] == data["id"], db[table]))) == 0:
+                        db[key].append(data)
+                else:
+                    return "[ERROR] WRONG COLUMNS FOR THAT TABLE", 403 
+            else:
+                return "INTERNAL SERVER ERROR", 500
+        return "INTERNAL SERVER ERROR", 500
+    
+    def put(self, table, key):
+        """
+        """
+        if table not in db.keys():
+            return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 500
+
+        json_data = request.get_json(force=True)
+        if json_data is not None:
+            ik = list(json_data.keys())[0]
+            data = json_data[ik]
+            if type(data) == dict:
+                if data.keys() == db[key][0].keys():
+                    if len(list(filter(lambda x:["id"] == data["id"], db[table]))) == 1:
+                        data["id"] = list(filter(lambda x:["id"] != data["id"], db[table]))
+                else:
+                    return "[ERROR] WRONG COLUMNS FOR THAT TABLE", 403 
+            else:
+                return "INTERNAL SERVER ERROR", 500
+        return "INTERNAL SERVER ERROR", 500
+
+
+    def delete(self, table, key):
+        """
+            Delete the tuple with id=$key from the table=$table 
+        """
+        if table in db.keys():
+            id = 'id'
+            removed = list(filter(lambda x: (x[id] == key), db[table]))
+            if len(removed) > 0:
+                db[table] = list(filter(lambda x: (x[id] != key), db[table]))
+            return 200
+        else:
+             return "[ERROR] "+ key + " TABLE DOESN'T EXISTS", 500
+
     
 
 class ApiServer:
